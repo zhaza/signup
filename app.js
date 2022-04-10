74baa94b93721c365b5856fca8bbb85ec4805577
zhaza <Khail.ballin12@yahoo.com>
2022-04-09 18:47:05 -0700

    Finished password validation

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
+  , confirm_password = document.getElementById("confirm_password")
+  , button = document.querySelector("button");
 
 function validatePassword(){
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.style.borderColor = "red";
+    password.style.borderColor = "red";
   } else {
     confirm_password.setCustomValidity('');
   }
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+button.onclick = validatePassword;
